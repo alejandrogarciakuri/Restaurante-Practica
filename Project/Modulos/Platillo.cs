84b611ade61e00ebf700b2913e85b2dee720ea36
Alejandro Garcia Kuri <15234404@modelo.edu.mx>
2023-12-19 00:42:42 -0600

    Now you can use class Platillo
    Added Modules folder and created class for Platillo

---
+++ Project/Modulos/Platillo.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Project.Interfaces;
+
+namespace Project.Modulos
+{
+    public class Platillo : IPlatillo
+    {
+        private string _nombre;
+        public string Nombre
+        {
+            get { return _nombre; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new Exception("El nombre del platillo no puede estar vacío o ser puros espacios en blanco.");
+                }
+
+                _nombre = value;
+            }
+        }
+
+        private int _precio;
+        public int Precio
+        {
+            get { return _precio; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception("El precio del platillo siempre debe ser positivo y mayor a 0.");
+                }
+
+                _precio = value;
+            }
+        }
+
+        private int _id;
+        public int ID
+        {
+            get { return _id; }
+            private set
+            {
+                _id = ID;
+            }
+        }
+
+        public static int LastIDAdded = 0;
+
+        public Platillo(string nombre, int precio, int id)
+        {
+            Nombre = nombre;
+            Precio = precio;
+            ID = LastIDAdded + 1;
+            LastIDAdded++;
+        }
+    }
+}
