86d0982b947fba7029c72206b0bb98864b557d07
JUANITEZ123 <148462093+JUANITEZ123@users.noreply.github.com>
2023-12-19 02:52:31 -0600

    Now can use the interface IMenu
    Add the interface IElementoMenu

---
+++ Project/Interfaces/IElementoMenu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Interfaces
+{
+    public interface IElementoMenu
+    {
+      string Nombre { get; set; }
+    int Precio { get; set; }
+
+        void MostrarElemento();
+    }
+}
